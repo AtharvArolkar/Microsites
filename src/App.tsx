2693c7b43c010b53b614854d438179790a15b8a9
atharv-arolkar_psl  <atharv_arolkar@persistent.com>
2024-05-30 12:50:53 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -98,12 +98,12 @@ function App() {
             <th scope="col" className="px-6 py-3">
               Created By
             </th>
-            {/* <th scope="col" className="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Created On
-            </th> */}
-            {/* <th scope="col" className="px-6 py-3">
+            </th>
+            <th scope="col" className="px-6 py-3">
               Status
-            </th> */}
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -119,12 +119,12 @@ function App() {
                 <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                   {item.createdBy}
                 </td>
-                {/* <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                   {item.createdTs}
-                </td> */}
-                {/* <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                </td>
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                   {getStatus(item.status)}
-                </td> */}
+                </td>
               </tr>
             );
           })}
